cfc15534e32ee4ad03db29fc7961457ff1b124c7
shimamoto-g1 <caldina222-123@yahoo.co.jp>
2020-09-19 00:22:58 +0900

    DrinkRecord　edit画面作成中9/19

--- app/src/main/java/jp/shimamoto/drinkrecord/MainActivity.kt
+++ app/src/main/java/jp/shimamoto/drinkrecord/MainActivity.kt
@@ -1,5 +1,6 @@
 package jp.shimamoto.drinkrecord
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
@@ -17,6 +18,10 @@ class MainActivity : AppCompatActivity() {
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
+
+            ////
+            val intent = Intent(this,EditActivity::class.java)
+            startActivity(intent)
         }
     }
 
